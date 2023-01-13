c07b61c926717191ccfd1bd21cb4d0ae4b971d18
Rashid <82169411+rash1dab2llaev@users.noreply.github.com>
2023-01-13 20:40:31 +0500

    Add files via upload

---
+++ final_project/js/script.js
@@ -0,0 +1,9 @@
+
+let typed = new Typed('.element', {
+    strings: ['Проверить по,^1000\n госномеру  ^2000\n VIN - коду или<br>  номеру кузова'],
+    typeSpeed: 70,
+    backSpeed: 20,
+    loop: true
+});
+
+
