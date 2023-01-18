524b922caa11636880ceb3f56ab7131e025cc188
Rashid Abdullaev <rash1d.ab2llaev@gmail.com>
2023-01-18 19:13:52 +0500

    ad

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-let b = document.querySelector('#b')
+let btn = document.querySelector('#btn')
 let main = document.querySelector('.main_block')
 let priceBlock = document.querySelector('.price_block')
 
-b.addEventListener('click', d)
+btn.addEventListener('click', filter)
 main.classList.add('filter')
 priceBlock.classList.add('filter')
 
-function d() {
-    if (b) {
+function filter() {
+    if (btn) {
         main.classList.remove('filter')
         priceBlock.classList.remove('filter')
     }
