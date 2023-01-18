e3e2fd8fb1ac59e9e8ad6c8fb9491a6885814f0c
Rashid Abdullaev <rash1d.ab2llaev@gmail.com>
2023-01-18 19:21:15 +0500

    adas

--- js2/script.js
+++ js2/script.js
@@ -1,3 +1,4 @@
+//filter
 let btn = document.querySelector('#btn')
 let main = document.querySelector('.main_block')
 let priceBlock = document.querySelector('.price_block')
@@ -14,29 +15,38 @@ function filter() {
 }
 
 
-
 //price block hover
 let price1 = document.querySelector('#price1')
 let price2 = document.querySelector('#price2')
 let price3 = document.querySelector('#price3')
+
 
 price1.addEventListener('click', add1)
 price2.addEventListener('click', add2)
 price3.addEventListener('click', add3)
 
 function add1() {
-    price1.classList.add('cl1')
-    price2.classList.remove('cl2')
-    price3.classList.remove('cl3')
 
+    price1.classList.add('dark')
+    price2.classList.remove('dark')
+    price3.classList.remove('dark')
+    price1.classList.remove('light')
+    price2.classList.add('light')
+    price3.classList.add('light')
 }
 function add2() {
-    price2.classList.add('cl2')
-    price1.classList.remove('cl1')
-    price3.classList.remove('cl3')
+    price2.classList.add('dark')
+    price1.classList.remove('dark')
+    price3.classList.remove('dark')
+    price1.classList.add('light')
+    price2.classList.remove('light')
+    price3.classList.add('light')
 }
 function add3() {
-    price3.classList.add('cl3')
-    price1.classList.remove('cl1')
-    price2.classList.remove('cl2')
+    price3.classList.add('dark')
+    price1.classList.remove('dark')
+    price2.classList.remove('dark')
+    price1.classList.add('light')
+    price2.classList.add('light')
+    price3.classList.remove('light')
 }
